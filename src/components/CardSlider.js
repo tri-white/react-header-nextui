9a11e42bfd85aed816db84e37805c63899e99f03
tri-white <olegovic101@gmail.com>
2023-12-10 23:20:51 +0200

    added highlight when changing video card

--- src/components/CardSlider.js
+++ src/components/CardSlider.js
@@ -1,5 +1,6 @@
 import React, { useState, useRef } from "react";
 import "../App.css";
+
 export default function CardSlider() {
   const [isVideoPlaying, setVideoPlaying] = useState(false);
   const [currentPage, setCurrentPage] = useState(1); // Initialize with page 1
@@ -17,9 +18,10 @@ export default function CardSlider() {
   const handleVectorClick = (direction) => {
     if (direction === "left" && currentPage > 1) {
       setCurrentPage(currentPage - 1);
-    } else if (direction === "right") {
+    } else if (direction === "right" && currentPage < 4) {
       setCurrentPage(currentPage + 1);
     }
+    
   };
 
   return (
@@ -31,31 +33,43 @@ export default function CardSlider() {
         </video>
       )}
       <>
-        <img className="Rectangle2" src={require("../img/rectangle-2.png")} />
+        <img
+          className={`Rectangle2 ${currentPage === 4 ? "highlight" : ""}`}
+          src={require("../img/rectangle-2.png")}
+        />
         {!isVideoPlaying && (
-          <img className="Rectangle3" src={require("../img/rectangle-3.png")} />
+          <img
+            className={`Rectangle3 ${currentPage === 3 ? "highlight" : ""}`}
+            src={require("../img/rectangle-3.png")}
+          />
         )}
-        <img className="Rectangle4" src={require("../img/rectangle-4.png")} />
-        <img className="Rectangle5" src={require("../img/rectangle-5.png")} />
+        <img
+          className={`Rectangle4 ${currentPage === 2 ? "highlight" : ""}`}
+          src={require("../img/rectangle-4.png")}
+        />
+        <img
+          className={`Rectangle5 ${currentPage === 1 ? "highlight" : ""}`}
+          src={require("../img/rectangle-5.png")}
+        />
         {!isVideoPlaying && (
           <div className="Ellipse16" onClick={handleEllipseClick}>
             <img className="playButton" src={require("../img/polygon.png")} />
           </div>
         )}
       </>
-      <div className="Ellipse5" onClick={() => handleVectorClick("left")}/>
+      <div className="Ellipse5" onClick={() => handleVectorClick("left")} />
       <img
         className="VectorLeft"
         src={require("../img/vectorLeft.png")}
         onClick={() => handleVectorClick("left")}
       />
-          <div className="Ellipse8"  onClick={() => handleVectorClick("right")}/>
+      <div className="Ellipse8" onClick={() => handleVectorClick("right")} />
       <img
         className="VectorRight"
         src={require("../img/vectorRight.png")}
         onClick={() => handleVectorClick("right")}
       />
-      <div className="PageNow">{String(currentPage).padStart(2, '0')}</div>
+      <div className="PageNow">{String(currentPage).padStart(2, "0")}</div>
     </div>
   );
 }
